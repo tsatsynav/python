b6aa846f61e546eb4aee2353bc23051b6315f21a
Andrey Tsatsyn <tsatsynav@yandex.ru>
2025-04-17 17:20:45 +0300

    3.4 - added

--- task1.py
+++ task1.py
@@ -1,3 +1,42 @@
-print('Hello, World!')
-name = input()
-print('Hi, ' + name)
+# print('Hello, World!')
+# name = input()
+# print('Hi, ' + name)
+# secondName = input('Input second name: ')
+# print('Hello', secondName)
+
+# num1, num2, num3 = 1, 2, 3
+
+# string1 = 'start'
+
+# print(num1, num2, num3, string1 + '... приехали')
+
+# a = 5
+# b = 10
+# print(a, b)
+# c = a
+# a = b
+# b = c
+# print(a, b)
+# x, y = 100, 500
+# print(x, y)
+# x, y = y, x
+# print(x, y)
+
+# a, b, c = 8, 5, 3
+# z = ((a / (b + c)) - a) / c
+# print(z)
+
+# bus = 5
+# metro = 3
+# result = (6 + (10 - bus) ** 2) / (metro + 1) + 132 / (2 + bus)
+# print(result)
+
+# a = (-3 + ((8 ** 2) - 12) * (4 ** (3 ** 2))) / (2 * 18)
+# print(a)
+
+num_1 = int(input('Input num 1: '))
+num_2 = int(input('Input num 2: '))
+num_3 = int(input('Input num 3: '))
+num_4 = int(input('Input num 4: '))
+total_value = (2 * ((num_3 + 5 + ((num_1 * num_2) / (4 * num_2))) * (num_4 - 2 * ((num_1 ** 3) / 30)))) - 10
+print(total_value)
